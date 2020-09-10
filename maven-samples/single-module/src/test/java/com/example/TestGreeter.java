f6cba7e6590db7d9af5ec91327681eb0d6855d7a
Jayanta, Chaudhuri <j.chaudhuri@hotmail.com>
2020-09-10 21:09:44 +0530

    Changes done to test the maven jenkins - git integrated schedule build testgit add .

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -37,4 +37,12 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+
+  @Test
+    public void greetShouldIncludeGreetingMessage()
+    {
+	    String someone = "World, Thanks you making me Happy Always!!!";
+	    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+		      
+    }
 }
