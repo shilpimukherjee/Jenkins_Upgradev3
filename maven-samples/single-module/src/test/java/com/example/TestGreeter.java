d4c4883d3b508493891f51aa454c7fa4ecfa9fe6
Jayanta, Chaudhuri <j.chaudhuri@hotmail.com>
2020-09-10 21:35:17 +0530

    Fixing build error

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -39,7 +39,7 @@ public class TestGreeter {
   }
 
   @Test
-    public void greetShouldIncludeGreetingMessage()
+    public void greetShouldIncludeGreetingMessageDisplay()
     {
 	    String someone = "World, Thanks you making me Happy Always!!!";
 	    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
